f2b9b514ad97eeb676dbf6a44d3942be27b08ab5
ya332 <alparslanyigitcan@gmail.com>
2020-12-26 19:26:13 -0500

    Run brute force search experiment and save results for df = 64 and ihls = 64

--- src/main.py
+++ src/main.py
@@ -9,9 +9,20 @@ import time
 
 def main(args):
     args = parse_arguments(args)
-    # run_entire_pipeline_for_brute_force_search(**args) # 1178.3585526 seconds with df = 8, ihls = 48
-    # run_entire_pipeline_for_diagonal_search(**args) # 530.6440734 seconds with df = 8, ihls = 48
-    run_entire_pipeline_for_zigzag_search(**args)
+    # Some runs for the brute force pipeline
+    # 1178.3585526 seconds with df = 8, ihls = 48
+    # 3854.4821924 seconds with df = 64 and ihls = 64
+    run_entire_pipeline_for_brute_force_search(**args) 
+    
+    # Some runs for the diagonal search pipeline
+    # 530.6440734 seconds with df = 8, ihls = 48
+    # TODO seconds with df = 64 and ihls = 64
+    run_entire_pipeline_for_diagonal_search(**args) 
+    
+    # Some runs for the zigzag search pipeline
+    # 240.3445642 seconds with df = 64, ihls = 48
+    # TODO seconds with df = 64 and ihls = 64
+    run_entire_pipeline_for_zigzag_search(**args) 
 
 if __name__ == "__main__": 
     
